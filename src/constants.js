687b087e173a254e0a0fd195121d6e74094ff497
lgxnders <alexander.stasyna@gmail.com>
2026-04-29 10:37:09 -0400

    Refactorings. Moving to Vite to bundle.

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const YEAR   = 518400;
+export const MONTH  = 43200;
+export const DAY    = 1440;
+export const WEEK   = 10080;
+export const HOUR   = 60;
+
+export const SILVER = 100;
+export const GOLD   = 10000;
